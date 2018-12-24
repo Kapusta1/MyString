33e21f04188b0aa8e827b446eb4804ca4a9dfc4c
tom <tomestrader@gmail.com>
2018-12-25 00:12:07 +0300

    Add Move constructor and operator assignment to move, and operator +

--- MyString.cpp
+++ MyString.cpp
@@ -6,9 +6,14 @@ class MyString
 {
 private:
 	char * Buffer;
+	MyString() :Buffer(NULL)
+	{
+		cout << "Default constructor called " << endl;
+	}
 public:
 	MyString(const char * InitialInput)
 	{
+		cout << "Constructor called for: " << InitialInput << endl;
 		if (InitialInput != NULL)
 		{
 			int len = strlen(InitialInput) + 1;
@@ -18,8 +23,9 @@ public:
 		else
 			Buffer = NULL;
 	}
-	MyString(const MyString &CopySource)
+	MyString(const MyString &CopySource)           //constructor copy
 	{
+		cout << "Copy constructor to copy from: " << CopySource.Buffer << endl;
 		if (CopySource.Buffer != NULL)
 		{
 			if (Buffer != NULL)
@@ -32,8 +38,9 @@ public:
 			Buffer = NULL;
 	}
 
-	MyString & operator =(const MyString & CopySource)
+	MyString & operator =(const MyString & CopySource) //operator assignment copy
 	{
+		cout << "Copy assignment operator to copy from: " << CopySource.Buffer << endl;
 		if ((this != &CopySource) && (CopySource.Buffer != NULL))
 		{
 			if (Buffer != NULL)
@@ -41,6 +48,26 @@ public:
 			int len = strlen(CopySource.Buffer) + 1;
 			Buffer = new char[len];
 			strcpy_s(Buffer, len, CopySource.Buffer);
+		}
+		return *this;
+	}
+	MyString(MyString && MoveSource)                    //Move constructor
+	{
+		cout << "Move constructor to move frome: " << MoveSource.Buffer << endl;
+		if (MoveSource.Buffer != NULL)
+		{
+			Buffer = MoveSource.Buffer;
+			MoveSource.Buffer = NULL;
+		}
+	}
+	MyString&operator=(MyString && MoveSource)        //operator assignment to move
+	{
+		cout << "Move assignment operator to move frome: " << MoveSource.Buffer << endl;
+		if ((MoveSource.Buffer != NULL) && (this != &MoveSource))
+		{
+			delete Buffer;
+			Buffer = MoveSource.Buffer;
+			MoveSource.Buffer = NULL;
 		}
 		return *this;
 	}
@@ -56,6 +83,19 @@ public:
 		if ((Buffer != NULL) && (index <= len))
 			return Buffer[index];
 	}
+	MyString operator + (const MyString & AddThis)
+	{
+		cout << "operator + called: " << endl;
+		MyString NewString;
+		if (AddThis.Buffer != NULL)
+		{
+			int AddLen = strlen(AddThis.Buffer) + 1;
+			NewString.Buffer = new char[GetLength() + AddLen];
+			strcpy_s(NewString.Buffer, strlen(Buffer) + 1, Buffer);
+			strcat_s(NewString.Buffer, strlen(Buffer) + AddLen, AddThis.Buffer);
+		}
+		return NewString;
+	}
 	~MyString()
 	{
 		if (Buffer != NULL)
@@ -67,22 +107,11 @@ public:
 
 int main()
 {
-	cout << "Type a statement";
-	string strInput;
-	getline(cin, strInput);
+	MyString Hello("Hello");
+	MyString World(" World");
+	MyString CPP(" of C++");
 
-	MyString yousaid(strInput.c_str());
-	cout << "Using operator [] for display your input: " << endl;
-	for (int Index = 0; Index < yousaid.GetLength(); ++Index)
-		cout << yousaid[Index] << " ";
-	cout << endl;
-
-	cout << "Enter index 0- " << yousaid.GetLength() - 1 << ": ";
-	int InIndex = 0;
-	cin >> InIndex;
-	cout << "Input character at sero-based position: " << InIndex;
-	cout << " is: " << yousaid[InIndex] << endl;
-	yousaid[5] = 't';
-
+	MyString sayHelloAgain(" overwrite this");
+	sayHelloAgain = Hello + World + CPP;
 	return 0;
 }
